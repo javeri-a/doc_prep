5f48c9ee343107a987f88546793b0aae1cf2ef78
javeri-a <javeriaqureshi746@gmail.com>
2025-08-13 23:39:26 +0500

    adding

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def main():
     print("Hello from docprep!")
+    print("Hello from docprep!")
+    print("Hello from docprep!")
 
     print("User: Where is the Golden Gate Bridge?")
     result = Runner.run(agent, "Where is the Golden Gate Bridge?", session=session)
